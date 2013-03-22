5e67cb48f681c3b4a1d5e3986ee00f4de797cb3e
howete <thowe@hp.com>
2013-03-22 17:36:13 -0600

    docs-36

---
+++ jenkins/merge.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+#
+# This script merges master to all the branches
+#
+rm -f ${SERVERS_DIR}/*/active
+git branch -r | grep -v origin/HEAD | grep -v origin/develop |
+while read BRANCH ROL
+do
+  echo "##### ${BRANCH} #####"
+  BRANCH=$(echo ${BRANCH} | sed -s 's,origin/,,')
+  git checkout -b "${BRANCH}" || git checkout -f "${BRANCH}"
+  git pull origin "${BRANCH}"
+  git merge origin "${BRANCH}" || true
+  git push origin "${BRANCH}" || true
+done
+exit 0
