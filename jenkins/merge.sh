2140428a94f0a861f66c8803074668b0a177d32d
Terry Howe <terrylhowe@gmail.com>
2013-11-04 09:34:00 -0700

    latest rebase changes

--- jenkins/merge.sh
+++ jenkins/merge.sh
@@ -9,12 +9,12 @@ rm -f ${SERVERS_DIR}/*/active
 git branch -r | grep -v origin/HEAD | grep -v origin/develop | grep -v origin/master |
 while read BRANCH ROL
 do
-  echo "##### ${BRANCH} #####"
   BRANCH=$(echo ${BRANCH} | sed -s 's,origin/,,')
+  echo "##### ${BRANCH} #####"
   git checkout -b "${BRANCH}" || git checkout -f "${BRANCH}"
   git reset HEAD || true
-  git pull origin "${BRANCH}"
+  git pull origin "${BRANCH}" </dev/null
   git rebase master
-  git push origin "${BRANCH}"
+  git push -f origin "${BRANCH}"
 done
 exit 0
