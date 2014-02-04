7f8abb09e72c42c8264aa7c639328e82a10cf194
Terry Howe <terrylhowe@gmail.com>
2014-02-04 08:40:36 -0700

    merge

--- jenkins/merge.sh
+++ jenkins/merge.sh
@@ -3,15 +3,16 @@
 # This script merges master to all the branches
 #
 rm -f ${SERVERS_DIR}/*/active
+git config --global push.default matching
 git branch -r | grep -v origin/HEAD | grep -v origin/develop | grep -v origin/master |
 while read BRANCH ROL
 do
   BRANCH=$(echo ${BRANCH} | sed -s 's,origin/,,')
   echo "##### ${BRANCH} #####"
-  git checkout -b "${BRANCH}" || git checkout -f "${BRANCH}"
+  git checkout -t "origin/${BRANCH}" 2>/dev/null || git checkout -f "${BRANCH}"
   git reset HEAD || true
   git pull origin "${BRANCH}"
-  git merge origin "${BRANCH}" || true
-  git push origin "${BRANCH}" || true
+  git rebase master || true
+  git push origin || true
 done
 exit 0
