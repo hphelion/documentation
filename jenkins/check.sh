12c0ef5d517ccf7e2a129b67fd388d79b90c6fcc
Greg Larsen <greg.larsen@hp.com>
2014-05-15 15:46:53 +0200

    initial commit

---
+++ jenkins/check.sh
@@ -0,0 +1,52 @@
+#!/bin/bash -li
+
+
+#Get the most recent version of the master branch
+env | grep GIT
+git checkout master
+git pull
+
+
+#Delete any tempfiles left over from the last run and write introduction
+rm checktmp > /dev/null 2>&1  
+echo ' Checking for embarassing strings...'    
+
+
+#Set Internal Field Separator to % (to preserve white space at the beginning and end of badstrings)
+IFS='%'
+
+
+#Read badstrings and pipe into loop
+cat ./jenkins/badstrings.txt |
+while read BAD
+do
+
+     #Do a recursive grep on all md files for the badstring and assign result to RESULT
+     RESULT=`grep -r --include="*.md" --exclude-dir=jenkins "${BAD}" ./`
+
+
+     #If RESULT is not empty, then write the bad string and the result to stout, and write 1 to the file checktmp
+     if [ -z "$RESULT" ]
+          then
+          EXIT=""
+     else
+          echo ""
+          echo "===$BAD============================="
+          echo "$RESULT"
+          echo "1" > checktmp
+     fi
+
+done  
+
+
+#Read chcktemp and assign content to EXIT (indicating that at least one error was found)
+EXIT=`cat checktmp` > /dev/null 2>&1
+
+
+#Exit script with 1 if an error was found.  Otherwise exit with 0.
+if [ -z "$EXIT" ]
+     then
+          exit 0
+     else
+          exit 1
+fi
