9e87c0838c73d23d23613d623d05193ce8f43931
Greg Larsen <greg.larsen@hp.com>
2014-09-18 21:56:20 +0200

    remove workbook from master branch.  backup sent to Jayme Perlman

--- devplatform/workbook/helloWorld/hello-world-php/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-// Writing and configuring PHP apps to run on the Helion Development Platform
-// is very easy. All you need is an index.php file and a manifest.yml with
-// your configuration options.
-echo "\n Hello World \n";
