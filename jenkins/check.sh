eed1e80c3c0dee028b17c2463909fbf21a4ceba5
Greg Larsen <greg.larsen@hp.com>
2014-10-13 17:10:25 +0200

    improved link checker

--- jenkins/check.sh
+++ jenkins/check.sh
@@ -35,7 +35,7 @@ echo  ""
 echo "==== Links to files that don't exist ==="
 for i in `find . -name "*.md" `
 do
-sed ':a;N;$!ba;s/\n/ /g'  $i | sed 's|-->|-->\n|g' | sed 's|<!--.*-->||g' | grep "](/.*)" | sed 's/.*](//' | sed 's/).*//' | sed 's|#.*||' | grep -v "/api/" | grep -v "^/file/" >> permalinklist.txt
+sed ':a;N;$!ba;s/\n/ /g'  $i | sed 's|-->|-->\n|g' | sed 's|<!--.*-->||g' | grep "](/.*)" | sed 's/.*](//' | sed 's/).*//' | sed 's|#.*||' | grep -v "/api/" | grep -v "^/file/" | sed 's|\/\/|\/|'>> permalinklist.txt
 
 grep permalink $i | sed 's|.* /|/|'  >> filepermalink.txt
 
