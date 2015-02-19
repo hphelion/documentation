808dbf15a807f8010a3555ac95ad447b23614f40
Greg Larsen <greg.larsen@hp.com>
2015-02-19 20:20:40 +0100

    error correction

--- jenkins/copy_publish-ready_files_to_master.sh
+++ jenkins/copy_publish-ready_files_to_master.sh
@@ -40,7 +40,7 @@ MDFILES_NOT_DESIGNATED=""
 for i in `find . -name "*.md" `
 do 
 
-	if [[ -n $(grep -vL "\-\-PUBLISH|\-\-UNDER REVISION" $i) ]]; 
+	if [[ -n $(grep -L "\-\-UNDER REVISION" $i) ]] && [[ -n $(grep -L "\-\-PUBLISH" $i) ]]; 
 	then
 	MDFILES_NOT_DESIGNATED=$MDFILES_NOT_DESIGNATED$s`echo $i`
 	fi
@@ -67,7 +67,7 @@ YMLFILES_NOT_DESIGNATED=""
 for i in `find . -name "*.yml" `
 do 
 
-	if [[ -n $(grep -vl "#PUBLISH|#UNDER REVISION" $i) ]]; 
+	if [[ -n $(grep -L "#PUBLISH" $i) ]] && [[ -n $(grep -L "#UNDER REVISION" $i) ]]; 
  
 	then
 		 
