1e6e3931413574e5d4f273bc0381e5bd65964eea
Greg Larsen <greg.larsen@hp.com>
2015-02-24 10:47:15 +0100

    test versioning links

--- addlink.sh
+++ addlink.sh
@@ -12,6 +12,8 @@ if [[ -e $newversion ]]
 then
 oldversion=$i
 newpermalink=`grep "^permalink:"  $newversion | sed 's:permalink.* /::' `
+
+
 #echo $newpermalink
 
 oldpermalink=`grep "^permalink:"  $oldversion | sed 's:permalink.* /::' `
@@ -21,13 +23,23 @@ oldpermalink=`grep "^permalink:"  $oldversion | sed 's:permalink.* /::' `
 #	Insert the 1.1 permalink into the 1.0 file as a link right after the title.
 
 	#find the title line number
-	echo $oldversion
-	awk '/^#[^#]/ {print FNR}' $oldversion
- echo " "
+
+	insertinOLDversion=`awk '/^#[^#]/ {print FNR}' $oldversion`
+	insertinOLDversion=$(($insertinOLDversion+1))
+	
+	
+	insertinNEWversion=`awk '/^#[^#]/ {print FNR}' $newversion`
+	insertinNEWversion=$(($insertinNEWversion+1))
+	 
+ 
+	sed -i "${insertinOLDversion} i \[See version 1.1 of this page\]\(${newpermalink}\)" $oldversion 
+ echo "changed $oldversion"
 #	Insert the 1.0 permalink into the 1.1 file as a link right after the title.
 
+sed -i "${insertinNEWversion} i \[See version 1.0 of this page\]\(${oldpermalink}\)" $newversion 
+ echo "changed $newversion"
 
-
+ exit 1
 else
 x=2
 #echo no 1.1 file found for $i
