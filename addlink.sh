3fd9aa23adcb3cd0cf0642ffcf05e7a05444e909
Greg Larsen <greg.larsen@hp.com>
2015-02-24 14:56:55 +0100

    added link to 1.1/1.0 document pairs to let users toggle between versions

--- addlink.sh
+++ addlink.sh
@@ -10,38 +10,40 @@ newversion=`echo $i | sed 's/1\.0/1\.1/'`
 
 if [[ -e $newversion ]]
 then
+ 
 oldversion=$i
 newpermalink=`grep "^permalink:"  $newversion | sed 's:permalink.* /::' `
 
 
- echo $newpermalink
+ #echo $newpermalink
 
 oldpermalink=`grep "^permalink:"  $oldversion | sed 's:permalink.* /::' `
-echo $oldpermalink
+#echo $oldpermalink
  echo ""
  
 #	Insert the 1.1 permalink into the 1.0 file as a link right after the title.
 
 	#find the title line number
 
-	insertinOLDversion=`awk '/^#[^#]/ {print FNR}' $oldversion`
+ 
+	insertinOLDversion=`awk '/^#[^#]/ {print FNR}' $oldversion | head -1`
 	insertinOLDversion=$(($insertinOLDversion+1))
 	
-	
-	insertinNEWversion=`awk '/^#[^#]/ {print FNR}' $newversion`
+ 
+	insertinNEWversion=`awk '/^#[^#]/ {print FNR}' $newversion  | head -1`
 	insertinNEWversion=$(($insertinNEWversion+1))
 
  
 	sed -i "${insertinOLDversion} i \[See version 1.1 of this page\]\(\/${newpermalink}\)" $oldversion 
- echo "changed $oldversion"
+ echo "Added link to $oldpermalink in the file $newversion"
 #	Insert the 1.0 permalink into the 1.1 file as a link right after the title.
 
 sed -i "${insertinNEWversion} i \[See version 1.0 of this page\]\(\/${oldpermalink}\)" $newversion 
- echo "changed $newversion"
-	  exit 1
+ echo "Added link to $newpermalink in the file $oldversion"
+	 
 
 else
-x=2
+x=2 
 #echo no 1.1 file found for $i
 fi
 
