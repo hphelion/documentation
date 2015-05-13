8f3dd98e60ed3a733cb132e795d7cba757739053
nancy mitchell <nancy.mitchell@hp.com>
2015-05-12 23:01:25 -0400

    hplinux

--- commercial/GA1/hlinuxpics.js
+++ commercial/GA1/hlinuxpics.js
@@ -1,29 +1,11 @@
-﻿
-var divs=document.getElementsByClassName("imgs");
-for (var i=1; i<divs.length+1; i++){
-var theDiv=document.getElementById("div" + i);
-theDiv.addEventListener("click", show);
-box.addEventListener("click", hide);
-}
-
-//put backgroun image name as style and change it
-
-
-function show(){
-
-//box.style.top="190px";
-//box.style.left="290px";
-box.style.visibility="visible";
-box.style.height="600px";
-
-var diagram=this.getAttribute("data-img");
-box.style.backgroundImage = "url('" + diagram + "')";
-}
-
-function hide(){
-
-document.getElementById("box").style.height="0px";
-document.getElementById("box").style.visibility="hidden";
+﻿function show(sp){
+  document.getElementById(sp).style.display="block";
+  document.getElementById(sp + 'a').style.display="none";
 
 }
 
+function hide(sp){
+  document.getElementById(sp).style.display="none";
+  document.getElementById(sp + 'a').style.display="block";
+
+}
