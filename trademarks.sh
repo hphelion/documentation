2398500616988ae60bfb9388323e8c72604f1218
Greg Larsen <greg.larsen@hp.com>
2015-06-26 16:37:58 +0200

    add line for cloud foundry

--- trademarks.sh
+++ trademarks.sh
@@ -2,6 +2,7 @@ for i in `find . -name "*.dita"`
 do
 
 sed -i 's|<tm tmtype="reg">\([A-Za-z ]*\)</tm>|\1|g' $i
+sed -i 's|<tm tmtype="tm">\([A-Za-z ]*\)</tm>|\1|g' $i
 sed -i 's|OpenStack(R)|OpenStack|g' $i
 
 sed -i '0,/OpenStack/{s/OpenStack/\<tm tmtype=\"reg\"\>OpenStack\<\/tm\>/}' $i
@@ -12,5 +13,6 @@ sed -i '0,/Mac OS X/{s/Mac OS X/\<tm tmtype=\"reg\"\>Mac OS X\<\/tm\>/}' $i
 sed -i '0,/Windows/{s/Windows/\<tm tmtype=\"reg\"\>Windows\<\/tm\>/}' $i 
 sed -i '0,/Xeon/{s/Xeon/\<tm tmtype=\"reg\"\>Xeon\<\/tm\>/}' $i 
 sed -i '0,/vSphere/{s/vSphere/\<tm tmtype=\"reg\"\>vSphere\<\/tm\>/}' $i
+sed -i '0,/Cloud Foundry/{s/Cloud Foundry/\<tm tmtype=\"tm\"\>Cloud Foundry\<\/tm\>/}' $i
 
 done
