4e96e2eb9aed6e74ded8b84352af425be5c33061
Greg Larsen <greg.larsen@hp.com>
2015-07-28 14:10:18 +0200

    bugfix

--- jenkins/oxygen-webhelp-build.sh
+++ jenkins/oxygen-webhelp-build.sh
@@ -108,7 +108,7 @@ TRANSTYPE=webhelp
 
 # The path of the directory of the input DITA map file
 # DITA_MAP_BASE_DIR=/home/test/oxygen-webhelp/OxygenXMLEditor/samples/dita/flowers
-DITA_MAP_BASE_DIR="./"
+DITA_MAP_BASE_DIR=`pwd`
 
 echo DITA_MAP_BASE_DIR
 echo $DITA_MAP_BASE_DIR
