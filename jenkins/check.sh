eceaad287cf9e4d3eb7f8ea1058e894bc0399c96
Greg Larsen <greg.larsen@hp.com>
2015-08-18 16:01:00 +0200

    corrections

--- jenkins/check.sh
+++ jenkins/check.sh
@@ -24,7 +24,7 @@ do
 		help=`echo $line | sed 's|.*=||' `
 		if [[ -n $(cat $i |sed ':a;N;$!ba;s/\n/ /g'| perl -pe  's|<!--.*-->||g' | grep "$test") ]]; 
 		then
-		cat $i |sed ':a;N;$!ba;s/\n/ /g'| perl -pe  's|<!--.*-->||g' | grep "$test"
+		#cat $i |sed ':a;N;$!ba;s/\n/ /g'| perl -pe  's|<!--.*-->||g' | grep "$test"
 			echo -e "${cc_blue}Found in file: $i on the $GIT_BRANCH (the issue is surrounded by ${cc_red}##${cc_blue}):${cc_normal}"
 			issue=`grep -n "$test" $i  | sed "s|\($test\)|${cc_red}##\1##${cc_normal}|" | sed 's|^\s||'`
 			echo -e "     ${cc_blue}Line number and text${cc_normal}: $issue"
