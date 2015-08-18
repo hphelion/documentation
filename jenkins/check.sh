8e4a7f51da6b9f3cc4f64fbf5f87f23a2d13a425
Greg Larsen <greg.larsen@hp.com>
2015-08-18 16:10:31 +0200

    corrections

--- jenkins/check.sh
+++ jenkins/check.sh
@@ -26,7 +26,7 @@ do
 		then
 		#cat $i |sed ':a;N;$!ba;s/\n/ /g'| perl -pe  's|<!--.*-->||g' | grep "$test"
 			echo -e "${cc_blue}Found in file: $i on the $GIT_BRANCH (the issue is surrounded by ${cc_red}##${cc_blue}):${cc_normal}"
-			issue=`grep -n "$test" $i  | sed "s|\($test\)|${cc_red}## \1i ##${cc_normal}"`
+			issue=`grep -n "$test" $i  | sed "s|\($test\)|${cc_red}## \1 ##${cc_normal}|"`
 			echo -e "     ${cc_blue}Line number and text${cc_normal}: $issue"
 			echo -e "     ${cc_blue}Correction${cc_normal}:  $help"
 			echo -e " "
