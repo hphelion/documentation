e5de5f223a2d5b6fc864dc07b789ac7bade023f9
Greg Larsen <greg.larsen@hpe.com>
2015-09-30 16:59:22 +0200

    redface improvements

--- jenkins/check.sh
+++ jenkins/check.sh
@@ -1,9 +1,6 @@
 #!/bin/bash 
 
-
 BRANCH=`echo $GIT_BRANCH | sed 's|.*\/||'`
-
-
 
 echo -e "===Looking for typos and words that should be avoided============================="
 
@@ -17,9 +14,6 @@ cc_yellow="${esc}[0;33m"
 cc_blue="${esc}[0;34m"
 cc_normal=`echo -en "${esc}[m\017"`
 
- 
-
-
 for i in `find . -name "*.dita"`
 do
 	while IFS='' read -r line  || [[ -n "$line" ]]
@@ -31,7 +25,7 @@ do
 		then
 		#cat $i |sed ':a;N;$!ba;s/\n/ /g'| perl -pe  's|<!--.*-->||g' | grep "$test" | grep "$except"
 			echo -e "${cc_blue}Found in file: $i on the $GIT_BRANCH (the issue is surrounded by ${cc_red}##${cc_blue}):${cc_normal}"
-			issue=`grep -n "$test" $i  | sed "s|\($test\)|${cc_red} ## \1 ## ${cc_normal}|"`
+			issue=`grep -n "$test" $i | grep -v "$except"  | sed "s|\($test\)|${cc_red} ## \1 ## ${cc_normal}|"`
 			echo -e "${cc_blue}Line number and text${cc_normal}:"
 			echo "$issue"
 			echo -e "${cc_blue}Correction${cc_normal}:  $help"
