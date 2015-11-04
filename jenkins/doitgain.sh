f21bf139b8615f2b53a51976a20d670d71f5cde9
joelbrandenburg <joel.brandenburg@hp.com>
2015-11-04 13:45:21 -0600

    redface

--- jenkins/doitgain.sh
+++ jenkins/doitgain.sh
@@ -4,7 +4,7 @@ for i in `find . -name "*.md"`
 
 do
 
-sed -i 's:^\[See version 1\.0 of this page\]:\[See the Helion OpenStack 1.0 version of this page\]:' $i
-sed -i 's:^\[See version 1\.1 of this page\]:\[See the Helion OpenStack 1.1 version of this page\]:' $i
+sed -i 's:^\[See version 1\.0 of this page\]:\[See the HPE Helion OpenStack 1.0 version of this page\]:' $i
+sed -i 's:^\[See version 1\.1 of this page\]:\[See the HPE Helion OpenStack 1.1 version of this page\]:' $i
 
 done
